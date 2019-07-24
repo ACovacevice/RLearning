2270e27417f8912daa2db10955fa3484ee17d433
Allan Covacevice <allan.covacevice@gmail.com>
2019-07-23 22:49:41 -0300

    1st push

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # RLearning
 Code being developed while studying the book "Reinforcement Learning: An Introduction", by Richard A. Sutton and Andrew G. Barto.
+
+### Chapter 2
+The k-armed bandit model.
+
+<figure>
+    <img src="img/karmedbandit_actions.png">
+    <p>Figure 2.1. A randomized 10-armed bandit. Each action rewards according to a gaussian distribution.</p>
+</figure>
+
+<figure>
+    <img src="img/karmedbandit_evaluation.png">
+    <p>Figure 2.2. Results from 2000 runs of 1000 steps each for varying &epsilon; values.</p>
+</figure>
---
+++ ch2/karmedbandit.py
@@ -0,0 +1,111 @@
+
+# coding: utf-8
+
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt # Required version 3.0.1 or later
+
+mpl.rcParams["font.size"] = 12
+prop_cycle = plt.rcParams['axes.prop_cycle']
+colors = prop_cycle.by_key()['color']
+
+class KArmedBandit:
+    
+    """
+    The k-armed bandit model.
+    Each action has probability p of being random, and (1 - p) of being greedy. 
+    Rewards come from randomized gaussian distributions.
+    """
+    
+    def __init__(self, k, mean=0, variance=1.0):
+        self.k = k
+        self.reset()
+        self.mean_values = np.random.normal(mean, variance, size=k)
+        
+        # Plots reward distributions
+        fig, ax = plt.subplots(1, k, figsize=(10, 6), constrained_layout=True, sharex=True, sharey=True)
+        ax[0].set_ylabel("Reward distribution")
+        [ax[i].hist(x=np.random.normal(loc=self.mean_values[i], size=5000), bins=50, 
+                    orientation='horizontal', color=colors[i]) for i in range(k)]
+        [(ax[i].set_xticks([]), ax[i].set_xlabel("Action %i" % (i+1))) for i in range(k)]
+        fig.suptitle("The %i-armed bandit" % k)
+        plt.show()
+    
+    def reset(self):
+        # Resets counters and rewards
+        self.avg_reward = list()
+        self.Q = np.zeros(shape=(self.k,))
+        self.N = np.zeros(shape=(self.k,))
+        self.optimal = np.array([])
+    
+    def get_reward(self, action):
+        # Computes the reward
+        reward = np.random.normal(loc=self.mean_values[action], scale=1.0)
+        
+        # Updates action counts and action values
+        self.N[action] += 1
+        self.Q[action] = self.Q[action] + (1 / self.N[action]) * (reward - self.Q[action])
+        
+        # Updates the (weighted) average reward
+        p = self.N / self.N.sum()
+        avg_reward = (self.Q * p).sum() / p.sum()
+        self.avg_reward.append(avg_reward)
+    
+    def rob(self, p, steps=1000):
+        # Plays for a certain number of steps
+        self.reset()
+        t = 1
+        while t <= steps:
+            step = np.random.choice(["greedy", "random"], p=[1-p, p], size=1)
+            if step == "greedy":
+                # Break any possible ties randomly
+                action = np.random.choice([a for a, x in enumerate(self.Q) if x == self.Q.max()])
+            elif step == "random":
+                action = np.random.choice(list(range(self.k)))
+            if action == self.mean_values.argmax():
+                self.optimal = np.append(self.optimal, 1)
+            else:
+                self.optimal = np.append(self.optimal, 0)
+            self.get_reward(action)
+            t += 1
+            
+    def rob_n_times(self, n=100, steps=1000, p=0.1, verbose=False):
+        # Plays n times a certain number of steps
+        for robbery in range(n):
+            self.rob(p, steps)
+            if robbery == 0:
+                mean_Q = np.array(self.avg_reward)
+                mean_O = self.optimal
+            else:
+                mean_Q += np.array(self.avg_reward)
+                mean_O += self.optimal
+        if verbose:
+            fig, ax = plt.subplots(1, 1)
+            ax.plot(range(steps), mean_Q)
+            ax.set_xlabel("Steps"), ax.set_ylabel("Average reward")
+            plt.show()
+        else:
+            return mean_Q / n, mean_O / n
+        
+if __name__ == "__main__":
+    
+    bandit = KArmedBandit(k=10)
+    
+    q0, opt0 = bandit.rob_n_times(n=2000, steps=1000, p=0.1)
+    q1, opt1 = bandit.rob_n_times(n=2000, steps=1000, p=0.01)
+    q2, opt2 = bandit.rob_n_times(n=2000, steps=1000, p=0.0)
+    
+    fig, (ax, ax1) = plt.subplots(1, 2, figsize=(12, 4), constrained_layout=True, sharex=True)
+
+    ax.plot(q0, color="blue", label="$\epsilon = 0.10$")
+    ax.plot(q1, color="red", label="$\epsilon = 0.01$")
+    ax.plot(q2, color="green", label="$\epsilon = 0.00$")
+
+    ax1.plot(opt0, color="blue", label="$\epsilon = 0.10$")
+    ax1.plot(opt1, color="red", label="$\epsilon = 0.01$")
+    ax1.plot(opt2, color="green", label="$\epsilon = 0.00$")
+
+    ax.set_xlabel("Steps"), ax1.set_xlabel("Steps")
+    ax.set_ylabel("Average Reward"), ax1.set_ylabel("Optimal Action (%)")
+    ax.legend()
+    plt.show()
