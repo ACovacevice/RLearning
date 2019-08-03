3ef00ec16d1beff312986ce26a5da06ffb8247da
Allan Covacevice <allan.covacevice@gmail.com>
2019-08-03 14:05:39 -0300

    More plots included

--- README.md
+++ README.md
@@ -7,11 +7,21 @@ Code being developed while studying the book "Reinforcement Learning: An Introdu
 Some results for the <i>"sample-averaged"</i>, <i>&epsilon;-greedy</i> 10-armed bandit model are presented below:
 
 <p align="center">
-    <img src="img/ch2/karmedbandit_actions.png" width="85%" height="85%"><br>
-    <b>Figure 2.1.</b> A randomized 10-armed bandit. Each action rewards according to a gaussian distribution.
+    <img src="img/ch2/Figure_2.1.png" width="100%" height="100%"><br>
+    <b>Figure 2.1.</b> Rewards for each action in a 10-armed bandit.
 </p>
 
 <p align="center">
-    <img src="img/ch2/karmedbandit_evaluation.png"><br>
+    <img src="img/ch2/Figure_2.2.png"><br>
     <b>Figure 2.2.</b> Results from 2000 runs of 1000 steps each for varying &epsilon; values.
+</p>
+
+<p align="center">
+    <img src="img/ch2/Figure_2.3.png"><br>
+    <b>Figure 2.3.</b> Results from 2000 runs of 1000 steps each. <a style="color:blue";>Blue:</a> greedy steps with optimistic initial values; <a style="color:red">Red:</a> &epsilon;-greedy steps.
+</p>
+
+<p align="center">
+    <img src="img/ch2/Figure_2.4.png"><br>
+    <b>Figure 2.4.</b> Results from 2000 runs of 1000 steps each. <a style="color:blue";>Blue:</a> upper-confidence bound greedy steps; <a style="color:grey">Grey:</a> &epsilon;-greedy steps.
 </p>
--- ch2/karmedbandit.py
+++ ch2/karmedbandit.py
@@ -1,11 +1,11 @@
 
 # coding: utf-8
 
+import os
 import numpy as np
 import matplotlib as mpl
-import matplotlib.pyplot as plt # Required version 3.0.1 or later
+import matplotlib.pyplot as plt # Required version 3.0.3 or later
 
-mpl.rcParams["font.size"] = 12
 prop_cycle = plt.rcParams['axes.prop_cycle']
 colors = prop_cycle.by_key()['color']
 
@@ -13,23 +13,29 @@ class KArmedBandit:
     
     """
     The k-armed bandit model.
-    Each action has probability p of being random, and (1 - p) of being greedy. 
     Rewards come from randomized gaussian distributions.
     """
     
-    def __init__(self, k, mean=0, variance=1.0):
+    def __init__(self, k, mean=0.0, variance=1.0, save=False):
         self.k = k
-        self.reset()
         self.mean_values = np.random.normal(mean, variance, size=k)
-        
+        self.save = save
         # Plots reward distributions
-        fig, ax = plt.subplots(1, k, figsize=(10, 6), constrained_layout=True, sharex=True, sharey=True)
-        ax[0].set_ylabel("Reward distribution")
-        [ax[i].hist(x=np.random.normal(loc=self.mean_values[i], size=5000), bins=50, 
-                    orientation='horizontal', color=colors[i]) for i in range(k)]
-        [(ax[i].set_xticks([]), ax[i].set_xlabel("Action %i" % (i+1))) for i in range(k)]
+        mpl.rcParams["font.size"] = 12
+        fig, ax = plt.subplots(1, 1, figsize=(10, 6), constrained_layout=True, sharex=True, sharey=True)
+        plots = ax.violinplot([np.random.normal(loc=self.mean_values[i], size=1000) for i in range(k)])
+        for i, body in enumerate(plots["bodies"]):
+            body.set_facecolor(colors[i - 10 if k > 10 else i])
+            body.set_alpha(0.5)
+        ax.set_xticks(range(1, k+1))
+        ax.set_xticklabels(["%i" % i for i in range(1, k+1)])
         fig.suptitle("The %i-armed bandit" % k)
-        plt.show()
+        ax.set_xlabel("Action")
+        ax.set_ylabel("Reward distribution")
+        if save:
+            fig.savefig(os.path.join(save, "Figure_2.1.png"), dpi=fig.dpi)
+        else:    
+            plt.show()
     
     def reset(self):
         # Resets counters and rewards
@@ -39,74 +45,121 @@ class KArmedBandit:
         self.avg_rewards = list()
         self.optimal = np.array([])
     
+    def get_action(self, p, step, ucb):
+        
+        # The epsilon-greedy (exploring) action
+        if np.random.rand() < p:
+            return np.random.choice(list(range(self.k)))
+        
+        # The Upper-Confidence-Bound action
+        if ucb > 0:
+            best_q = self.Q + ucb * np.sqrt(np.log(step) / (self.N + 1e-6))
+            return np.random.choice(np.where(best_q == best_q.max())[0])
+        
+        # The greedy action
+        return np.random.choice(np.where(self.Q == self.Q.max())[0])
+    
     def get_reward(self, action, step):
         # Computes the reward
         reward = np.random.normal(loc=self.mean_values[action], scale=1.0)
         
         # Updates action counts and action values
         self.N[action] += 1
-        self.Q[action] += (1 / self.N[action]) * (reward - self.Q[action])
+        step_size = 1 / self.N[action] if self.alpha is None else self.alpha
+        self.Q[action] += step_size * (reward - self.Q[action])
         
         # Updates the average reward
         self.avg_reward += (reward - self.avg_reward) / step
         self.avg_rewards.append(self.avg_reward)
+        
+        # Updates optimal action count
+        if action == self.mean_values.argmax():
+            self.optimal = np.append(self.optimal, 1)
+        else:
+            self.optimal = np.append(self.optimal, 0)
     
-    def rob(self, p, steps=1000):
+    def rob(self, p, steps=1000, ucb=0, initial=0):
         # Plays for a certain number of steps
         self.reset()
+        self.Q += initial
         t = 1
         while t <= steps:
-            step = np.random.choice(["greedy", "random"], p=[1-p, p], size=1)
-            if step == "greedy":
-                # Break any possible ties randomly
-                action = np.random.choice([a for a, x in enumerate(self.Q) if x == self.Q.max()])
-            elif step == "random":
-                action = np.random.choice(list(range(self.k)))
-            if action == self.mean_values.argmax():
-                self.optimal = np.append(self.optimal, 1)
-            else:
-                self.optimal = np.append(self.optimal, 0)
+            action = self.get_action(p, t, ucb)
             self.get_reward(action, t)
             t += 1
             
-    def rob_n_times(self, n=100, steps=1000, p=0.1, verbose=False):
+    def rob_n_times(self, n=100, steps=1000, p=0.1, alpha=None, ucb=0, initial=0):
         # Plays n times a certain number of steps
+        self.alpha = alpha
         for robbery in range(n):
-            self.rob(p, steps)
+            self.rob(p, steps, ucb, initial)
             if robbery == 0:
                 avg_R = np.array(self.avg_rewards)
                 avg_optimal = self.optimal
             else:
                 avg_R += np.array(self.avg_rewards)
                 avg_optimal += self.optimal
-        if verbose:
-            fig, ax = plt.subplots(1, 1)
-            ax.plot(range(steps), avg_R)
-            ax.set_xlabel("Steps"), ax.set_ylabel("Average reward")
+        return avg_R / n, avg_optimal / n
+
+    def plot_figure_2_2(self):
+        # Reproduces and plots the computation of Figure 2.2 in the book. 
+        mpl.rcParams["font.size"] = 12
+        q0, opt0 = self.rob_n_times(n=2000, steps=1000, p=0.1)
+        q1, opt1 = self.rob_n_times(n=2000, steps=1000, p=0.01)
+        q2, opt2 = self.rob_n_times(n=2000, steps=1000, p=0.0)
+        fig, (ax, ax1) = plt.subplots(1, 2, figsize=(12, 4), constrained_layout=True, sharex=True)
+        ax.plot(q0, color="blue", label="$\epsilon = 0.10$")
+        ax.plot(q1, color="red", label="$\epsilon = 0.01$")
+        ax.plot(q2, color="green", label="$\epsilon = 0.00$")
+        ax1.plot(100 * opt0, color="blue")
+        ax1.plot(100 * opt1, color="red")
+        ax1.plot(100 * opt2, color="green")
+        ax.set_xlabel("Steps"), ax1.set_xlabel("Steps")
+        ax.set_ylabel("Average Reward"), ax1.set_ylabel("Optimal Action (%)")
+        ax.legend()
+        if self.save:
+            fig.savefig(os.path.join(save, "Figure_2.2.png"), dpi=fig.dpi)
+        else:    
             plt.show()
-        else:
-            return avg_R / n, avg_optimal / n
+        
+    def plot_figure_2_3(self):
+        # Reproduces and plots the computation of Figure 2.3 in the book: optimistic initial values.
+        mpl.rcParams["font.size"] = 12
+        q0, opt0 = self.rob_n_times(n=2000, steps=1000, p=0.0, alpha=0.1, initial=5)
+        q1, opt1 = self.rob_n_times(n=2000, steps=1000, p=0.1, alpha=0.1)
+        fig, (ax, ax1) = plt.subplots(1, 2, figsize=(12, 4), constrained_layout=True, sharex=True)
+        ax.plot(q0, color="blue", label="greedy:     Q+5")
+        ax.plot(q1, color="red", label="$\\epsilon$-greedy: $\\epsilon = 0.1$, Q+0")
+        ax1.plot(100 * opt0, color="blue")
+        ax1.plot(100 * opt1, color="red")
+        ax.set_xlabel("Steps"), ax1.set_xlabel("Steps")
+        ax.set_ylabel("Average Reward"), ax1.set_ylabel("Optimal Action (%)")
+        ax.legend()
+        if self.save:
+            fig.savefig(os.path.join(save, "Figure_2.3.png"), dpi=fig.dpi)
+        else:    
+            plt.show()
+    
+    def plot_figure_2_4(self):
+        # Reproduces and plots the computation of Figure 2.4 in the book: upper-confidence bound.    
+        mpl.rcParams["font.size"] = 12
+        q0, opt0 = self.rob_n_times(n=2000, steps=1000, p=0.1)
+        q1, opt1 = self.rob_n_times(n=2000, steps=1000, p=0.0, ucb=2.0)
+        fig, (ax, ax1) = plt.subplots(1, 2, figsize=(12, 4), constrained_layout=True, sharex=True)
+        ax.plot(q0, color="grey", label="$\epsilon$-greedy: $\epsilon = 0.1$")
+        ax.plot(q1, color="blue", label="UCB:        $c = 2.0$")
+        ax1.plot(100 * opt0, color="grey")
+        ax1.plot(100 * opt1, color="blue")
+        ax.set_xlabel("Steps"), ax1.set_xlabel("Steps")
+        ax.set_ylabel("Average Reward"), ax1.set_ylabel("Optimal Action (%)")
+        ax.legend()
+        if self.save:
+            fig.savefig(os.path.join(save, "Figure_2.4.png"), dpi=fig.dpi)
+        else:    
+            plt.show()
         
 if __name__ == "__main__":
-    
-    bandit = KArmedBandit(k=10)
-    
-    q0, opt0 = bandit.rob_n_times(n=2000, steps=1000, p=0.1)
-    q1, opt1 = bandit.rob_n_times(n=2000, steps=1000, p=0.01)
-    q2, opt2 = bandit.rob_n_times(n=2000, steps=1000, p=0.0)
-    
-    fig, (ax, ax1) = plt.subplots(1, 2, figsize=(12, 4), constrained_layout=True, sharex=True)
-
-    ax.plot(q0, color="blue", label="$\epsilon = 0.10$")
-    ax.plot(q1, color="red", label="$\epsilon = 0.01$")
-    ax.plot(q2, color="green", label="$\epsilon = 0.00$")
-
-    ax1.plot(100 * opt0, color="blue", label="$\epsilon = 0.10$")
-    ax1.plot(100 * opt1, color="red", label="$\epsilon = 0.01$")
-    ax1.plot(100 * opt2, color="green", label="$\epsilon = 0.00$")
-
-    ax.set_xlabel("Steps"), ax1.set_xlabel("Steps")
-    ax.set_ylabel("Average Reward"), ax1.set_ylabel("Optimal Action (%)")
-    ax.legend()
-    plt.show()
-    fig.savefig("../img/ch2/karmedbandit_evaluation.png", dpi=100)
+    bandit = KArmedBandit(k=10, save=os.path.join("..", "img", "ch2"))
+    bandit.plot_figure_2_2()
+    bandit.plot_figure_2_3()
+    bandit.plot_figure_2_4()
